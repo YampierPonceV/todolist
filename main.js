262ad4159832c8e34e72a8b8ed77da5c0e539d8f
Yampier Ponce <developer.yampier@gmail.com>
2021-12-04 13:11:00 -0500

    📝 refactor classlist active

--- main.js
+++ main.js
@@ -9,12 +9,17 @@ const form = $("form");
 const container = $("#container");
 const container_list = $("#container_list");
 const label = $(".label");
+const clearBtn = $("#clear");
+
 const arreglo = [];
 let html = "";
 
 function layout(text) {
   if (container_list.clientHeight > 500) {
     container.classList.add("container-list-scroll");
+  }
+  if (container_list.clientHeight < 500) {
+    container.classList.remove("container-list-scroll");
   }
   html = `
     <article class="card">
@@ -45,10 +50,14 @@ function render() {
 container.addEventListener("click", (e) => {
   if (e.target.tagName === "BUTTON") {
     e.target.parentElement.remove();
-    save();
   } else if (e.target.tagName === "LABEL") {
     e.target.classList.toggle("active");
+    /* if (e.target.classList.contains("active")) {
+      e.target.parentElement.classList.toggle("active");
+    } */
   }
+
+  save();
 });
 
 function mostrarList(e) {
@@ -64,5 +73,10 @@ function mostrarList(e) {
   input.value = "";
 }
 
-render();
 form.addEventListener("submit", mostrarList);
+render();
+
+clearBtn.addEventListener("click", () => {
+  console.log(container.innerHTML.includes("active"));
+  console.log(container.children);
+});
